3da1f69597a48add8480f908157eca3b05221980
Timo Petteri Sinnemäki <timo.sinnemaki@helsinki.fi>
2016-05-19 01:45:53 +0300

    Removed the fixed-output attributes from xephyr-launcher because I'm guessing they were pointless.

--- priv-sep-pkgs/xephyr-launcher/default.nix
+++ priv-sep-pkgs/xephyr-launcher/default.nix
@@ -6,10 +6,6 @@ pkgs.stdenv.mkDerivation {
   name = "xephyr-launcher-0.1.0.0";
   src = ./builddir;
   buildInputs = [pkgs.haskell.compiler.ghc7103];
-  outputHashAlgo = "sha512";
-  outputHashMode = "recursive";
-  outputHash = "39c57aa9d0d69c20b62bf6c2de814255bd7386ab7f11522982cd0b80d1ba3b57" +
-               "647d6db1ca4c6ecbfca37b59af64f334ea037de93c1c2d726056251a4805764b";
   buildPhase = ''
     export HOME=`pwd`
     cd src
